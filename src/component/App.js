b2dc60dc60da87ce4248e21d6ec6c1aec175081d
vikram1vicky@gmail.com <vikram1vicky@gmail.com>
2017-09-17 00:14:42 +0530

    nw

---
+++ src/component/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    };
+  }
+
+
+
+  render() {
+    return (
+      <div className="component-app">
+      Vikram
+      </div>
+    );
+  }
+}
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
+import App from './component/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
 // registerServiceWorker();
