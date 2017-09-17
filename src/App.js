8ceafaa3d8baca7b0d51e6141e8dbed415d5d431
vikram1vicky@gmail.com <vikram1vicky@gmail.com>
2017-09-17 14:19:01 +0530

    s

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <form id="generator">
+        <fieldset id="characterTypes">
+          <legend>Drag and Drop to re-arrange the following items:</legend>
+          <ul></ul>
+        </fieldset>
+        <output></output>
+      </form>
     );
   }
 }
