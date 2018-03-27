7584736fb87ef243beb66d4d913f4beaba23fc40
vikram1vicky@gmail.com <vikram1vicky@gmail.com>
2018-03-27 22:22:47 +0530

    changed version

--- src/About.js
+++ src/About.js
@@ -5,7 +5,7 @@ export class About extends Component {
     render() {
         return (<div className={`about-wrapper`}>
             <h1>About</h1>
-            <p><b>App Version:</b> 0.0.1.beta</p>
+            <p><b>App Version:</b> 0.0.1</p>
             <p><b>Developer:</b> Vikram Gupta</p>
             <p><b>Contact:</b> vikram1vicky@gmail.com</p>
         </div>);
